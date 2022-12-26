e301fa2fdeedd6d3e7c39d2e3f18fc3881612688
upgrace-in <thedesiretree47@gmail.com>
2022-12-26 11:56:15 +0530

    work

--- team_app/src/subComp/Calculator.js
+++ team_app/src/subComp/Calculator.js
@@ -5,27 +5,28 @@ export default function Calculator(props) {
 
 
     useEffect(() => {
-        props.calculator($('.loanAmount3'), $('.credits'))
+        props.calculator($('.loanAmount3'), $('.credits2'))
     }, [])
 
     return (
         <div className={props.formState === 'Calculator' ? 'show' : 'hide'}>
+            <h1>Calculator</h1>
+            <br/>
             <form className="wpcf7-form init">
-                <div className="comment-one__form row">
-                    <div className="col-xl-6">
-                        <div className="comment-form__input-box">
-                            <span className="wpcf7-form-control-wrap" data-name="your-name">
-                                <input className="loanAmount3"
-                                    style={{ fontSize: 1.5 + 'rem', borderRadius: 10 + 'px', padding: 10 + 'px', color: '#000' }} type="number"
-                                    value="100000" placeholder="Enter Amount" />
-                            </span>
+                <div className="comment-one__form">
+                    <div className='row'>
+                        <div className="col-xl-12">
+                            <div className="comment-form__input-box"><span className="wpcf7-form-control-wrap"
+                                data-name="your-phone"><input id="inputloanAmt" type="number" size="40"
+                                    className="loanAmount3 wpcf7-form-control wpcf7-text" aria-invalid="false"
+                                    placeholder="Loan Amount" /></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="comment-form__input-box">
-                            <span className="wpcf7-form-control-wrap" data-name="your-name">
-                                <h3>Credits: </h3>
-                            </span>
+                        <div className="col-xl-12">
+                            <div className="comment-form__input-box creditText">
+                                Credits: <b id="inputCredits" className="credits2">$0</b>
+                            </div>
+
                         </div>
                     </div>
                 </div>
